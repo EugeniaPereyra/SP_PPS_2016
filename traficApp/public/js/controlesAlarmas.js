7091547630f940d9a23be75f4f32a4b4183ab0fe
EugeniaPereyra <meugeniape@gmail.com>
2016-10-31 13:39:51 -0300

    firebase hosting

---
+++ traficApp/public/js/controlesAlarmas.js
@@ -0,0 +1,16 @@
+angular.module('alarma.controllers', [])
+
+.controller('alarmasCtrl', function($scope, $stateParams) {
+   $scope.Ambulancia = function() {
+    alert ("llegue");
+  };
+
+$scope.Accidente = function() {
+    alert ("llegue");
+  };
+
+$scope.AnimalSuelto = function() {
+    alert ("llegue");
+  };
+
+});
