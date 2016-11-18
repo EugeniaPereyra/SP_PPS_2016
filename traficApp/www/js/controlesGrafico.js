01ec9860d7996b43c2d8732dc9535c0d5824ed6b
sebasPagano <sebas_el14@live.com.ar>
2016-11-18 20:06:31 -0300

    recibiendo datos tipo accidente de firebase

--- traficApp/www/js/controlesGrafico.js
+++ traficApp/www/js/controlesGrafico.js
@@ -1,18 +1,37 @@
-angular.module('grafico.controllers', [])
+angular.module('grafico.controllers', ['ngCordova'])
 
-.controller('graficoCtrl', function($scope, Alarmas) {
+.controller('graficoCtrl', function($scope, Alarmas,$timeout) {
   // Sample options for first chart
-  var alarmas=[];
+  var alarmas= [];
   $scope.mecanico=0;
   $scope.animal=0;
   $scope.accidente=0;
   $scope.ambulancia=0;
 
+  
+
+  $scope.DatosFire = new Firebase("https://traficapp.firebaseio.com/alarmas");
+
+ $scope.DatosFire.on('child_added', function (snapshot) {
+   
+    var message = snapshot.val();
+    alarmas.push(message.tipo);
+
+    console.log("Adentro",alarmas);
+ 
+  $scope.mecanico=4;
+ 
+   
+
+  });
+console.log("Afuera",alarmas);
+console.log("Mecanico",$scope.mecanico)
+
 
 
     $scope.chartOptions = {
         title: {
-            text: 'Cantidad de alarmas cada 3 días'
+            text: 'Cantidad de alarmas'
           },
         xAxis: {
           title: {
