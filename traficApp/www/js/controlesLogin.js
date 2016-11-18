c03ed5073f6cea7194a74c187c3192250b889f56
sebasPagano <sebas_el14@live.com.ar>
2016-11-18 16:52:40 -0300

    Se saco el login con github, no funciona en el celular.

--- traficApp/www/js/controlesLogin.js
+++ traficApp/www/js/controlesLogin.js
@@ -31,49 +31,4 @@ angular.module('starter.controllers', ['ngCordova'])
   }
 
 
-  $scope.LoginGitHub = function() {
-  try{
-	$cordovaVibration.vibrate(200);
-   }
-   
-   catch(Exception){
-	console.log(Exception.Message);
-   }
-    $scope.Funciona = false;
-  $scope.NoFunciona = false;  
-  
-   var provider = new firebase.auth.GithubAuthProvider();
-
-   firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {      
-        var token = result.credential.accessToken;  
-      }
-     
-      var user = result.user;
-
-      $timeout(function(){
-      $scope.Funciona = true;
-      console.log("Respuesta: ", result);  
-      $state.go("app.mapa");
-      }); 
-
-    }).catch(function(error) {
-     
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-       $timeout(function(){
-        $scope.NoFunciona = true;
-        console.log("Error: ", error);
-       }); 
-    });
-
-
-  };
-
-
-
 });
